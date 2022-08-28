2b0f10fc9c618b527553dc2e58f5cda0a4e98358
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-08-28 18:49:36 +0900

    Feat : `CanvasView.kt`
    - 화면에 그려지는 것까지 완성

--- app/src/main/java/com/example/exhibitionguestbook/CanvasView.kt
+++ app/src/main/java/com/example/exhibitionguestbook/CanvasView.kt
@@ -3,13 +3,41 @@ package com.example.exhibitionguestbook
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.view.MotionEvent
 import android.view.View
+import android.view.ViewConfiguration
 import androidx.core.content.res.ResourcesCompat
 
 class CanvasView(context : Context) : View(context) {
+    companion object {
+        private const val STROKE_WIDTH = 12f
+    }
+
     private lateinit var extraCanvas : Canvas
     private lateinit var extraBitmap : Bitmap
+
     private val backgroundColor = ResourcesCompat.getColor(resources, R.color.white, null)
+    private val drawColor = ResourcesCompat.getColor(resources, R.color.black, null)
+    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
+
+    private var path = Path()
+    private var touchEventX = 0f
+    private var touchEventY = 0f
+    private var currentX = 0f
+    private var currentY = 0f
+
+    private val paint = Paint().apply {
+        color = drawColor
+        isAntiAlias = false
+        isDither = true
+
+        style = Paint.Style.STROKE
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+        strokeWidth = STROKE_WIDTH
+    }
 
     override fun onDraw(canvas: Canvas){
         super.onDraw(canvas)
@@ -19,8 +47,51 @@ class CanvasView(context : Context) : View(context) {
     override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
         if (::extraBitmap.isInitialized) extraBitmap.recycle()
         super.onSizeChanged(width, height, oldWidth, oldHeight)
+
         extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         extraCanvas = Canvas(extraBitmap)
         extraCanvas.drawColor(backgroundColor)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        touchEventX = event.x
+        touchEventY = event.y
+
+        when(event.action){
+            MotionEvent.ACTION_DOWN -> touchStart()
+            MotionEvent.ACTION_MOVE -> touchMove()
+            MotionEvent.ACTION_UP -> touchUp()
+        }
+
+        return true
+    }
+
+    private fun touchStart() {
+        path.reset()
+        path.moveTo(touchEventX, touchEventY)
+
+        currentX = touchEventX
+        currentY = touchEventY
+    }
+
+    private fun touchMove() {
+        val dx = Math.abs(touchEventX - currentX)
+        val dy = Math.abs(touchEventY - currentY)
+
+        if(dx >= touchTolerance || dy >= touchTolerance){
+            path.quadTo(currentX, currentY,
+                (touchEventX + currentX) / 2,
+                (touchEventY + currentY) / 2)
+
+            currentX = touchEventX
+            currentY = touchEventY
+            extraCanvas.drawPath(path, paint)
+        }
+
+        invalidate()
+    }
+
+    private fun touchUp() {
+        path.reset()
     }
 }
