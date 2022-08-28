68f422ac7e9a99788c08aafca398ce2e23e6b9db
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-08-28 19:46:27 +0900

    Feat : `MainActivity.kt`
    - viewBinding 적용

--- app/src/main/java/com/example/exhibitionguestbook/MainActivity.kt
+++ app/src/main/java/com/example/exhibitionguestbook/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.exhibitionguestbook
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import com.example.exhibitionguestbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
