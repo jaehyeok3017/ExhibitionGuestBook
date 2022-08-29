423e3c010de5dfd1a91a6b56022f4a21cd1d4b8f
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-08-29 23:38:12 +0900

    Feat : `MainActivity.kt`
    - 컬러팔레트를 눌렀을 때, 각 색깔이 CanvasView에 전달되도록 함

--- app/src/main/java/com/example/exhibitionguestbook/MainActivity.kt
+++ app/src/main/java/com/example/exhibitionguestbook/MainActivity.kt
@@ -1,16 +1,40 @@
 package com.example.exhibitionguestbook
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
 import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import android.view.ViewConfiguration
+import androidx.core.content.res.ResourcesCompat
 import com.example.exhibitionguestbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var canvasView : CanvasView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        canvasView = CanvasView(this, attrs = null)
+
+        binding.colorBlack.setOnClickListener { canvasView.drawColorSet("black") }
+        binding.colorBlue.setOnClickListener { canvasView.drawColorSet("blue") }
+        binding.colorGreen.setOnClickListener { canvasView.drawColorSet("green") }
+        binding.colorOrange.setOnClickListener { canvasView.drawColorSet("orange") }
+        binding.colorPink.setOnClickListener { canvasView.drawColorSet("pink") }
+        binding.colorRed.setOnClickListener { canvasView.drawColorSet("red") }
+
+        binding.erase.setOnClickListener { canvasView.erase() }
+        binding.delete.setOnClickListener { canvasView.reset() }
     }
 }
+
