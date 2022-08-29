49be46ecd6a3268aff4549505d6dda1361bfab00
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-08-29 23:39:07 +0900

    Feat : `CanvasView.kt`
    - 변수 위치 조정 및 변수 추가
    - 지우개 만들기 (약 50% 완성)
    - 색깔 변경 (완료)
    - 화면 갈아엎기 (살짝 조정 필요)

--- app/src/main/java/com/example/exhibitionguestbook/CanvasView.kt
+++ app/src/main/java/com/example/exhibitionguestbook/CanvasView.kt
@@ -1,44 +1,71 @@
 package com.example.exhibitionguestbook
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewConfiguration
+import android.widget.FrameLayout
 import androidx.core.content.res.ResourcesCompat
 
+
 class CanvasView(context : Context, attrs : AttributeSet?) : View(context, attrs) {
+    init{
+        setLayerType(FrameLayout.LAYER_TYPE_HARDWARE, null)
+    }
+
+    companion object {
+        private const val STROKE_WIDTH = 12f
+
+        private var strokePaint = Paint().apply {
+            color = Color.BLACK
+            isAntiAlias = true
+            isDither = true
+
+            style = Paint.Style.STROKE
+            strokeJoin = Paint.Join.ROUND
+            strokeCap = Paint.Cap.ROUND
+            strokeWidth = STROKE_WIDTH
+        }
+
+        private val eraseCirclePaint = Paint().apply {
+            isAntiAlias = true
+            style = Paint.Style.STROKE
+            strokeWidth = STROKE_WIDTH
+        }
+
+        private val erasePaint = Paint().apply {
+            isAntiAlias = true
+            xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
+        }
+    }
+
     private lateinit var extraCanvas : Canvas
     private lateinit var extraBitmap : Bitmap
 
     private val backgroundColor = ResourcesCompat.getColor(resources, R.color.white, null)
-    private val drawColor = ResourcesCompat.getColor(resources, R.color.black, null)
     private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
 
     private var path = Path()
     private var touchEventX = 0f
     private var touchEventY = 0f
+    private var erasePositionX = 0f
+    private var erasePositionY = 0f
     private var currentX = 0f
     private var currentY = 0f
+    private var isMoving = false
+    private var isErasing = false
 
-    private val paint = Paint().apply {
-        color = drawColor
-        isAntiAlias = false
-        isDither = true
 
-        style = Paint.Style.STROKE
-        strokeJoin = Paint.Join.ROUND
-        strokeCap = Paint.Cap.ROUND
-        strokeWidth = STROKE_WIDTH
-    }
 
     override fun onDraw(canvas: Canvas){
         super.onDraw(canvas)
         canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+
+        if(isErasing && isMoving){
+            canvas.drawCircle(erasePositionX, erasePositionY, STROKE_WIDTH, eraseCirclePaint)
+        }
     }
 
     override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
@@ -51,13 +78,41 @@ class CanvasView(context : Context, attrs : AttributeSet?) : View(context, attrs
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
+        if(event == null) return false
+
+        // S-pen
+        if(event.getToolType(0) != MotionEvent.TOOL_TYPE_STYLUS) return false
+        if(event.buttonState == MotionEvent.BUTTON_STYLUS_PRIMARY){
+            if(!isMoving) {
+                isErasing = !isErasing
+            }
+        }
+
         touchEventX = event.x
         touchEventY = event.y
 
         when(event.action){
-            MotionEvent.ACTION_DOWN -> touchStart()
-            MotionEvent.ACTION_MOVE -> touchMove()
-            MotionEvent.ACTION_UP -> touchUp()
+            MotionEvent.ACTION_DOWN -> {
+                if(isErasing){
+                    extraCanvas.drawCircle(touchEventX, touchEventY, STROKE_WIDTH, erasePaint)
+                    erasePositionX = touchEventX
+                    erasePositionY = touchEventY
+                }
+                else touchStart()
+            }
+            MotionEvent.ACTION_MOVE -> {
+                if(isErasing){
+                    path.addCircle(touchEventX, touchEventY, STROKE_WIDTH, Path.Direction.CW)
+                    erasePositionX = touchEventX
+                    erasePositionY = touchEventY
+                }
+                else touchMove()
+            }
+            MotionEvent.ACTION_UP -> {
+                if(isErasing) extraCanvas.drawPath(path, erasePaint)
+                else extraCanvas.drawPath(path, strokePaint)
+                touchUp()
+            }
         }
 
         return true
@@ -82,17 +137,47 @@ class CanvasView(context : Context, attrs : AttributeSet?) : View(context, attrs
 
             currentX = touchEventX
             currentY = touchEventY
-            extraCanvas.drawPath(path, paint)
+            extraCanvas.drawPath(path, strokePaint)
         }
 
         invalidate()
     }
 
     private fun touchUp() {
+        isMoving = false
+
+        erasePositionX = 0f
+        erasePositionY = 0f
+
         path.reset()
+        invalidate()
     }
 
-    companion object {
-        private const val STROKE_WIDTH = 12f
+    fun drawColorSet(changeColor: String){
+        isErasing = false
+
+        var nextColor : Int = when(changeColor){
+            "black" -> R.color.black
+            "red" -> R.color.red
+            "orange" -> R.color.orange
+            "green" -> R.color.green
+            "blue" -> R.color.blue
+            "pink" -> R.color.pink
+            else -> R.color.black
+        }
+
+        val resourceNextColor = ResourcesCompat.getColor(resources, nextColor, null)
+        strokePaint.color = resourceNextColor
+    }
+
+    fun erase() {
+        isErasing = true
+    }
+
+    fun reset() {
+        extraBitmap = Bitmap.createBitmap(2000, 2000, Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(backgroundColor)
+        invalidate()
     }
 }
