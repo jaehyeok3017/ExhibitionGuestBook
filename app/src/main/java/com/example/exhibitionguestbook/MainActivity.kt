917fb6693ada857cf7df9e1a37682135efb3d1b3
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-09-03 14:05:42 +0900

    Feat : `MainActivity.kt` All Remove Complete
    - 화면 전체 삭제 구현 완료
    - 추후 구현 : 정말로 삭제할 것인지 묻기

--- app/src/main/java/com/example/exhibitionguestbook/MainActivity.kt
+++ app/src/main/java/com/example/exhibitionguestbook/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.exhibitionguestbook
 import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.ContentValues.TAG
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Point
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.view.marginTop
 import com.example.exhibitionguestbook.databinding.ActivityMainBinding
 import com.google.firebase.storage.FirebaseStorage
 import java.io.ByteArrayOutputStream
@@ -17,6 +19,7 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var canvasView : CanvasView
+    private var previousColor = "black"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,23 +29,15 @@ class MainActivity : AppCompatActivity() {
         canvasView = CanvasView(this, attrs = null)
         binding.canvas.isDrawingCacheEnabled = true
 
-        binding.colorBlack.setOnClickListener { canvasView.drawColorSet("black") }
-        binding.colorBlue.setOnClickListener { canvasView.drawColorSet("blue") }
-        binding.colorGreen.setOnClickListener { canvasView.drawColorSet("green") }
-        binding.colorOrange.setOnClickListener { canvasView.drawColorSet("orange") }
-        binding.colorPink.setOnClickListener { canvasView.drawColorSet("pink") }
-        binding.colorRed.setOnClickListener { canvasView.drawColorSet("red") }
+        binding.colorBlack.setOnClickListener { canvasView.drawColorSet("black"); previousColor = "black" }
+        binding.colorBlue.setOnClickListener { canvasView.drawColorSet("blue"); previousColor = "blue" }
+        binding.colorGreen.setOnClickListener { canvasView.drawColorSet("green"); previousColor = "green" }
+        binding.colorOrange.setOnClickListener { canvasView.drawColorSet("orange"); previousColor = "orange" }
+        binding.colorPink.setOnClickListener { canvasView.drawColorSet("pink"); previousColor = "pink" }
+        binding.colorRed.setOnClickListener { canvasView.drawColorSet("red"); previousColor = "red" }
 
-        binding.erase.setOnClickListener { canvasView.drawColorSet("white") }
-        binding.reset.setOnClickListener {
-            val display = windowManager.defaultDisplay
-            val size = Point()
-            display.getRealSize(size)
-
-            val width = size.x
-            val height = size.y
-            canvasView.reset(width, height)
-        }
+        binding.erase.setOnClickListener { canvasView.drawColorSet("white"); previousColor = "white" }
+        binding.reset.setOnClickListener { startActivity(Intent(this, MainActivity::class.java)) }
 
         binding.upload.setOnClickListener {
             val storage: FirebaseStorage? = FirebaseStorage.getInstance()
@@ -53,6 +48,7 @@ class MainActivity : AppCompatActivity() {
 
             val baos = ByteArrayOutputStream()
             val bitmap = getBitmap()
+            Log.d(TAG, "imageWidth : ${bitmap.width}, imageHeight : ${bitmap.height}")
             bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
             val data = baos.toByteArray()
 
